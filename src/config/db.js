bd5e58d0e93132c070b9eb3366bede9f57ae3dcc
aryanboii <arya94189@gmail.com>
2026-09-08 11:22:13 +0530

    connection config

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,17 @@
 import pkg from "pg"
+const {Pool} = pkg;
+
+const pool = new Pool({
+    user:process.env.USER,
+    host:process.env.HOST,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port:process.env.PORT,
+})
+
+pool.on("connect",()=>{
+    console.log("connection pool establish with db");
+    
+})
+
+export default pool;
