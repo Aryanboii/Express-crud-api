37bbd2e66a18ebee22e17ae37af41a1577a07375
aryanboii <arya94189@gmail.com>
2026-09-10 01:42:23 +0530

    testing postgres

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js"
 
 dotenv.config();
 
@@ -16,6 +17,12 @@ app.use(cors());
 // Routes
 
 //Error handling middlewars
+
+//testing postgres connecion
+app.get("/" , async(req,res)=>{
+    const result = await pool.query("SELECT current_database()");
+    res.send("The database name is : ${result.rows[0].current_database}")
+})
 
 //server running
 
